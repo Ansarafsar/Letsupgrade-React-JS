c16fd7ce27e7a04c02f47775073b3dc3906c8d59
Ansar Afsar <83577572+Ansarafsar@users.noreply.github.com>
2021-07-15 21:11:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './style.css'
+import Button from "./Button"
+import { Color } from 'jest-matcher-utils/node_modules/chalk'
+
+
+const App = () => {
+    return ( <
+        div className = "container" >
+        <
+        h1 style = {
+            { textAlign: "center", color: "blue" } } > Hi!!!!this is dp < /h1> <
+        h2 > Welcome to our react Assignment < /h2> <
+        div className = "btn" >
+        <
+        Button buttonText = "rule-1" / >
+        <
+        Button buttonText = "rule-2" / >
+        <
+        Button buttonText = "rule-3" / >
+        <
+        /div>
+
+
+
+        <
+        /div>
+    )
+}
+
+export default App
