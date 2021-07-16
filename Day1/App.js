5dea9bb9af1e404dbe287188a61bbf2622d838d1
Ansar Afsar <83577572+Ansarafsar@users.noreply.github.com>
2021-07-16 19:00:03 +0530

    Create App.js

---
+++ Day1/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './style.css'
+import Button from "./Button"
+import { Color } from 'jest-matcher-utils/node_modules/chalk'
+
+
+const App = () => {
+    return(
+        <div className="container">
+            <h1 style={{textAlign:"center",color:"blue"}}>Hi! this is ansar</h1>
+            <h2>Welcome to our react Assignment</h2>
+            <div className="btn">
+                <Button buttonText="rule-1"/>
+                <Button buttonText="rule-2" />
+                <Button buttonText="rule-3"/>
+            </div>
+            
+            
+           
+        </div>
+    )
+}
+
+export default App
+© 2021 GitHub, Inc.
